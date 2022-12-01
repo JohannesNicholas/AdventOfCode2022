6dab9c106397f309542d5411831bc7439d7aa33d
JohannesNicholas <goanna7007@gmail.com>
2022-12-02 10:12:10 +1100

    Complete

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,42 @@
 //use the fs module
 use std::fs;
 
+
 fn main() {
+    //stage1();
+    stage2();
+}
+
+
+fn stage2() {
     println!("Day 1 challenge!");
 
     //take input from file
     let list = fs::read_to_string("input.txt").expect("Error reading file");
 
-    let mut greatest_total = 0;
+    //Greatest totals available
+    let mut greatest_totals = [0; 3];
+
+    //current total for the current elf
     let mut current_total = 0;
+
+    let mut lowest_index = 0;
 
 
     // Find the greatest total
     for line in list.lines() {
         //we've reached the end of a group
         if line.is_empty() {
-            // If the current total is greater than the greatest total, set the greatest total to the current total
-            if current_total > greatest_total {
-                greatest_total = current_total;
+            
+            //replace the lowest value in the greatest totals
+            if current_total > greatest_totals[lowest_index] {
+                greatest_totals[lowest_index] = current_total;
+
+                //greatest totals have changed so we need to find the new lowest index
+                lowest_index = get_lowest_index(greatest_totals);
             }
+
+            
 
             // Reset the current total
             current_total = 0;
@@ -28,11 +46,67 @@ fn main() {
         }
     }
 
+    //handle the last number
+    if current_total > greatest_totals[lowest_index] {
+        greatest_totals[lowest_index] = current_total;
+    }
+    
+
+    println!("Most: {:?}", greatest_totals);
+
+    // Calculate the final total
+    let mut final_total = 0;
+    for i in 0..greatest_totals.len() {
+        final_total += greatest_totals[i];
+    }
+
+    //output the greatest totals
+    println!("Final total: {}", final_total);
+}
+
+fn get_lowest_index(greatest_totals: [i32; 3]) -> usize {
+    let mut lowest_index = 0;
+    let mut lowest_value = greatest_totals[0];
+
+    for i in 1..greatest_totals.len() {
+        if greatest_totals[i] < lowest_value {
+            lowest_index = i;
+            lowest_value = greatest_totals[i];
+        }
+    }
+
+    lowest_index
+}
+
+
+
+
+
+fn stage1() {
+    println!("Day 1 challenge!");
+    //take input from file
+    let list = fs::read_to_string("input.txt").expect("Error reading file");
+    let mut greatest_total = 0;
+    let mut current_total = 0;
+    // Find the greatest total
+    for line in list.lines() {
+        //we've reached the end of a group
+        if line.is_empty() {
+            // If the current total is greater than the greatest total, set the greatest total to the current total
+            if current_total > greatest_total {
+                greatest_total = current_total;
+            }
+            // Reset the current total
+            current_total = 0;
+        } else {
+            // Add the number to the current total
+            current_total += line.parse::<i32>().unwrap();
+        }
+    }
     // To handle when the last elf has the most
     if current_total > greatest_total {
         greatest_total = current_total;
     }
-
     //output the greatest total
     println!("{}", greatest_total);
 }
