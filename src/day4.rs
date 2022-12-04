45bc9554f4c1ba7518d6129f8dcee4047ad5a7fc
JohannesNicholas <goanna7007@gmail.com>
2022-12-04 16:35:59 +1100

    completed day 4

--- src/day4.rs
+++ src/day4.rs
@@ -58,7 +58,60 @@ pub fn stage1() {
 
 pub fn stage2() {
     println!("Day 4 challenge! pt.2");
-   
+    
+    //take input from file
+    let list = fs::read_to_string("input.txt").expect("Error reading file");
+
+    //split input into lines
+    let lines = list.lines();
+
+    let mut counter = 0;
+
+    //loop through lines
+    for line in lines {
+        //get number before "-"
+        let number1 = line.split("-").next().unwrap();
+        //get number after "-" but before ","
+        let number2 = line.split("-").nth(1).unwrap().split(",").next().unwrap();
+        //get number after "," but before "-"
+        let number3 = line.split(",").nth(1).unwrap().split("-").next().unwrap();
+        //get number after second "-"
+        let number4 = line.split("-").nth(2).unwrap();
+
+        //convert numbers to u32
+        let number1 = number1.parse::<u32>().unwrap();
+        let number2 = number2.parse::<u32>().unwrap();
+        let number3 = number3.parse::<u32>().unwrap();
+        let number4 = number4.parse::<u32>().unwrap();
+
+        //number 1 to 2 is range 1
+        //number 3 to 4 is range 2
+
+        //if number 1 is inside range 2
+        if number1 >= number3 && number1 <= number4 {
+            counter += 1;
+        }
+
+        //if number 2 is inside range 2
+        else if number2 >= number3 && number2 <= number4 {
+            counter += 1;
+        }
+
+        //if number 3 is inside range 1
+        else if number3 >= number1 && number3 <= number2 {
+            counter += 1;
+        }
+
+        //if number 4 is inside range 1
+        else if number4 >= number1 && number4 <= number2 {
+            counter += 1;
+        }
+
+
+        
+    }
+
+    println!("Total: {}", counter);
 
     
 }
