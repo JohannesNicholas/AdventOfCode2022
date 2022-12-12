d59844a25c74c5934139868d220743b38b55f0b8
JohannesNicholas <goanna7007@gmail.com>
2022-12-12 15:59:54 +1100

    starting 12

---
+++ src/day12.rs
@@ -0,0 +1,43 @@
+use std::fs;
+
+
+pub fn stage1() {
+    println!("Day 12 challenge! pt.1");
+
+    //take input from file
+    let list = fs::read_to_string("input.txt").expect("Error reading file");
+
+    //get the lines
+    let mut lines = list.lines();
+
+    let mut count = 0;
+
+    for line in lines {
+
+    }
+
+
+    println!("TOTAL: {}", count);
+    
+}
+
+
+
+pub fn stage2() {
+    println!("Day 12 challenge! pt.2");
+
+    //take input from file
+    let list = fs::read_to_string("input.txt").expect("Error reading file");
+
+    //get the lines
+    let mut lines = list.lines();
+
+    let mut count = 0;
+
+    for line in lines {
+
+    }
+
+
+    println!("TOTAL: {}", count);
+}
--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,15 @@ use std::fs;
 //mod day5;
 //mod day6;
 //mod day7;
-mod day11;
+mod day12;
 
 
 fn main() {
     //stage1();
     //stage2();
     //day2stage1();
-    day11::stage1();
-    day11::stage2();
+    day12::stage1();
+    day12::stage2();
 }
 
 
